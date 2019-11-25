c2a2f0d30dcb16547c3d136258da85cd103c1d2b
Ashu <ashu.sharma@tothenew.com>
2019-11-25 12:26:07 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    instaData : []
+  };
+
+  async componentDidMount() {
+    let data = []
+    const url = 'https://www.instagram.com/bigboytoyz_india/';
+    const instaSource = await axios.get(url);
+    const instaObj = instaSource.data.match(
+        /<script type="text\/javascript">window\._sharedData =(.*)<\/script>/
+    )[1].slice(0,-1)
+
+    const convertedJsonObj = JSON.parse(instaObj)
+    console.log(convertedJsonObj);
+    const mediaArr = convertedJsonObj.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges
+
+    mediaArr.map(el=>{
+      const node = el.node
+      data.push(node)
+    })
+    this.setState({
+      instaData : data
+    })
+  }
+
+  renderInstaPics = () => {
+    return this.state.instaData.map((media , i)=>{
+      return(
+          <div style={{display: "inline"}} key={i}>
+            <img src={media.thumbnail_src} alt="insta" style={{width:"200px"}}/>
+          </div>
+      )
+    })
+  }
+
+  render() {
+        return (
+            <div>
+              {this.state.instaData ? this.renderInstaPics() : <h5> Nothing Found!!!! </h5> }
+            </div>
+        );
+    }
 }
 
 export default App;
